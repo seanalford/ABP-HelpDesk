bff2a8b0fef1ee3901a5e01b85a3382da7688064
sean.alford <sean.alford@icloud.com>
2020-05-19 11:16:18 -0400

    Added User Organization NP migration

---
+++ Acme.HelpDesk/src/Acme.HelpDesk.EntityFrameworkCore.DbMigrations/Migrations/20200519150307_Added_User_Organizatin_Navigation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Acme.HelpDesk.Migrations
+{
+    public partial class Added_User_Organizatin_Navigation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
